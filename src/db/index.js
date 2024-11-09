4178f6c05812f23be4131eb6fb4e23755aff5018
Jeet Mahapatra <149883188+jeet-mahapatra@users.noreply.github.com>
2024-11-09 21:20:26 +0530

    Update index.js uri password change

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js"
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(
-            `mongodb+srv://jeetvictus:jeet@cluster0.fty5p.mongodb.net/${DB_NAME}`  //process.env.MONGO_DB use karne me error araha he ------>>>>  MongoParseError: Invalid scheme, expected connection string to start with "mongodb://" or "mongodb+srv://"
+            `mongodb+srv://jeetvictus:<password>@cluster0.fty5p.mongodb.net/${DB_NAME}`  //process.env.MONGO_DB use karne me error araha he ------>>>>  MongoParseError: Invalid scheme, expected connection string to start with "mongodb://" or "mongodb+srv://"
         );
         console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`)
         
