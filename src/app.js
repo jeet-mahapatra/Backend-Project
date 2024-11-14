aa9a381bc61d462847d5f0729bbce48444e5529d
jeet-mahapatra <jeetmahapatra2004@gmail.com>
2024-11-14 20:46:38 +0530

    all setup configired

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
-import cors from cors
+import cors from "cors"
 import cookieParser from "cookie-parser"
+
 
 
 const app = express()
@@ -14,5 +15,17 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users" , userRouter) //  --> http://localhost:8000/api/v1/users/register
+
+
+
+
 
 export {app} 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import {app} from "./app.js"
 
 dotenv.config({
     path: "./.env"
@@ -13,11 +14,12 @@ dotenv.config({
 connectDB()
 .then(()=>{
     app.on("error: ",(error)=>{
-        console.log("error: ",error)
+        console.log("error",error)
         throw error
     })
-    app.listen(process.env.PORT || 8000 , ()=> {
-        console.log(`Server is running at port ; ${process.env.PORT }`)
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT || 8000 , ()=> {
+        console.log(`Server is running at port : ${PORT}`)
     }) 
     
 })
